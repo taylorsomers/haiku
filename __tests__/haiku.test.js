ebbb58473c79ee0afe06a4e42f1859ec3c05f096
Taylor Somers <tsomers@yandex.com>
2020-06-24 13:55:20 -0400

    Rewrite poem object constructor to include full lines of the poem rather than syllable counts, and rewrite failing test of whether the program constructs such a poem
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -2,10 +2,13 @@ import { Poem } from './../src/haiku.js';
 
 describe('Poem', () => {
 
-  test('should create a poem object with three line syllable counts', () => {
-    const poem = new Poem(1,2,3);
-    expect(poem.line1SyllableCount).toEqual(1);
-    expect(poem.line2SyllableCount).toEqual(2);
-    expect(poem.line3SyllableCount).toEqual(3);
+  test('should create a poem object with three arrays', () => {
+    const poem = new Poem();
+    expect(poem.lineOne).toEqual([]);
+    expect(poem.lineTwo).toEqual([]);
+    expect(poem.lineThree).toEqual([]);
   });
+  
+  // test('should ')
+  //tests whether the line contains any unwanted nonalphanum char
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,7 +1,17 @@
 export class Poem {
-  constructor(line1SyllableCount, line2SyllableCount, line3SyllableCount) {
-    this.line1SyllableCount = line1SyllableCount;
-    this.line2SyllableCount = line2SyllableCount;
-    this.line3SyllableCount = line3SyllableCount;
+  constructor() {
+    this.lineOne = [];
+    this.lineTwo = [];
+    this.lineThree = [];
   }
+  // unwanted() {
+  //   //access the line, method to go thru each char in the line, and flag unwanteds.
+    
+//   }
+//   addLine() {
+//     Poem_____.push(line1)
+//   }
+// }
+// function Line(){
+//   //user input line1
 }
