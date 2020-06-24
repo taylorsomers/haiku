5d9f79a1a9a410afeaddf6104a51c35d384b0ff6
Taylor Somers <tsomers@yandex.com>
2020-06-24 13:37:31 -0400

    Add passing test for whether the program creates a poem object with three line syllable counts
    Co-authored-by: Chris Yoon <chy-yoon@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -4,7 +4,7 @@ describe('Poem', () => {
 
   test('should create a poem object with three line syllable counts', () => {
     const poem = new Poem(1,2,3);
-    expect(poem.line1SyllableCount).toEqual(2);
+    expect(poem.line1SyllableCount).toEqual(1);
     expect(poem.line2SyllableCount).toEqual(2);
     expect(poem.line3SyllableCount).toEqual(3);
   });
