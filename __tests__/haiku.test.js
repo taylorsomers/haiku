9a87bd21b4e3e44c4a206a6c24db557b27a332f9
Taylor Somers <tsomers@yandex.com>
2020-06-24 17:54:37 -0400

    Add allStrings prototype method to concatenate all lines of the poem for analysis
    create failing test for allStrings prototype method
    
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

--- __tests__/haiku.test.js
+++ __tests__/haiku.test.js
@@ -1,27 +1,36 @@
 import { Poem } from './../src/haiku.js';
 
 describe('Poem', () => {
-
-  test('should create a poem object with three arrays', () => {
-    const poem = new Poem();
-    expect(poem.lineOne).toEqual([]);
-    expect(poem.lineTwo).toEqual([]);
-    expect(poem.lineThree).toEqual([]);
+  let reusablePoem;
+  
+  beforeEach(() => {
+    reusablePoem = new Poem();
   });
   
-  test('should populate poem object line properties with inputted content', () => {
-    let poem = new Poem();
-    poem.addLine("hi","bye","bro");
-    expect(poem.lineOne[0]).toEqual("hi");
-    expect(poem.lineTwo[0]).toEqual("bye");
-    expect(poem.lineThree[0]).toEqual("bro");
+  test('should create a reusablePoem object with three arrays', () => {
+    expect(reusablePoem.lineOne).toEqual([]);
+    expect(reusablePoem.lineTwo).toEqual([]);
+    expect(reusablePoem.lineThree).toEqual([]);
+  });
+  
+  test('should populate reusablePoem object line properties with inputted content', () => {
+    reusablePoem.addLine("hi","bye","bro");
+    expect(reusablePoem.lineOne[0]).toEqual("hi");
+    expect(reusablePoem.lineTwo[0]).toEqual("bye");
+    expect(reusablePoem.lineThree[0]).toEqual("bro");
   });
 
   test('should correctly identify if a character is a number', () => {
-    let poem = new Poem();
-    poem.lineOne[0] = "aslasdf5";
-    poem.lineTwo[0] = "4";
-    poem.lineThree[0] = "a";
-    expect(poem.isNumber()).toEqual("it is a number");
+    reusablePoem.lineOne[0] = "aslasdf5";
+    reusablePoem.lineTwo[0] = "4";
+    reusablePoem.lineThree[0] = "a";
+    expect(reusablePoem.isNumber()).toEqual("it is a number");
+  });
+
+  test('should concatenate all lines of the reusablePoem', () => {
+    reusablePoem.lineOne[0] = "aslasdf5";
+    reusablePoem.lineTwo[0] = "4";
+    reusablePoem.lineThree[0] = "a";
+    expect(reusablePoem.allStrings()).toEqual("");
   });
 });
--- src/haiku.js
+++ src/haiku.js
@@ -11,12 +11,23 @@ export class Poem {
     this.lineThree.push(inputLine3);
   }
 
-  isNumber() {
+  allStrings() {
     let allString = this.lineOne[0].concat(this.lineTwo[0], this.lineThree[0]);
+    return allString;
+  }
+
+  isNumber() {
+    let allString = this.allStrings();
     for(let element of allString) {
       if (!isNaN(parseInt(element))) {
         return "it is a number";
       }
     }
   }
+
+  // isWord() {
+  //   //loop through array until space and slice everything prior to space
+  //   // create a variable that is equal to the sliced off string
+  //   // check to see whether that variable/string is included in dictionary
+  // }
 }
