5de9fd6c6aa1968e97f37de48ad57deda6530081
Taylor Somers <tsomers@yandex.com>
2020-06-24 13:35:49 -0400

    Write failing test for whether the program will create a poem object with three line syllable counts
    Co-authored-by: Chris Yoon <chy.yoon@gmail.com>

---
+++ __tests__/haiku.test.js
@@ -0,0 +1,11 @@
+import { Poem } from './../src/haiku.js';
+
+describe('Poem', () => {
+
+  test('should create a poem object with three line syllable counts', () => {
+    const poem = new Poem(1,2,3);
+    expect(poem.line1SyllableCount).toEqual(2);
+    expect(poem.line2SyllableCount).toEqual(2);
+    expect(poem.line3SyllableCount).toEqual(3);
+  });
+});
---
+++ src/haiku.js
@@ -0,0 +1,7 @@
+export class Poem {
+  constructor(line1SyllableCount, line2SyllableCount, line3SyllableCount) {
+    this.line1SyllableCount = line1SyllableCount;
+    this.line2SyllableCount = line2SyllableCount;
+    this.line3SyllableCount = line3SyllableCount;
+  }
+}
